38e02ab63dcbf3af0209ebbc7b4fa19a304739c9
JackyLin2 <jacky.jia.lin@gmail.com>
2022-04-26 15:13:36 -0400

    admin

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
 
 dotenv.config();
 
@@ -11,10 +17,14 @@ mongoose.connect(
     process.env.MONGO_URL
 ).then(console.log("DB SUCESSFUL")).catch(((err) => console.log(err)))
 
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 
 
